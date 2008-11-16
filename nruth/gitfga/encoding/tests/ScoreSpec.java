31091a8a6783947d2360349e67adc1bc0bf03e22
Nicholas Rutherford <nick.rutherford@ardes.com>
2008-11-16 16:31:52 +0000

    implemented ordered list of notes spec for score. shuffled spec a little

--- nruth/gitfga/encoding/tests/ScoreSpec.java
+++ nruth/gitfga/encoding/tests/ScoreSpec.java
@@ -4,20 +4,53 @@
 package nruth.gitfga.encoding.tests;
 import static org.junit.Assert.*;
 
-import org.junit.Test;
+import nruth.gitfga.encoding.*;
+import nruth.gitfga.encoding.tests.NoteSpec.NoteFactory;
+
+import org.junit.*;
 
 /**
 	@author nicholasrutherford
 	
  */
 public class ScoreSpec {
+	private Score fixture;
+	Note[] fixture_notes = new Note[5];
+	@Before
+	public void setUp(){
+		for(int i=0; i<fixture_notes.length; i++){fixture_notes[i] = NoteFactory.getRandomNote();}
+		fixture = new Score(fixture_notes);
+	}
+
 	@Test
 	public void ordered_list_of_notes(){
+		//check notes match in order
+		for(int i=0; i<fixture_notes.length; i++){assertEquals(fixture_notes[i], fixture.getNote(i+1));}		
+	}
+	
+	@Test(expected=IndexOutOfBoundsException.class)
+	public void negative_list_index(){
+		fixture.getNote(-1);
+	}
+	
+	@Test(expected=IndexOutOfBoundsException.class)
+	public void zero_list_index(){
+		fixture.getNote(0);
+	}
+	
+	@Test(expected=IndexOutOfBoundsException.class)
+	public void off_end_list_index(){
+		fixture.getNote(fixture_notes.length+1);
+	}
+	
+	//next spec
+	@Test
+	public void indexed_by_time_played_from_start(){
 		fail("not implemented");
 	}
 	
 	@Test
-	public void indexed_by_time_played_from_start(){
+	public void note_durations_recorded(){
 		fail("not implemented");
 	}
 	
