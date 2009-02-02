bb766d48e3028ae89b28f714a5181844ea141feb
Nicholas Rutherford <nick.rutherford@ardes.com>
2009-02-02 00:33:47 +0000

    initial work on getting GA working for monophonic C major scale, starting with 'feature' spec.

---
+++ nruth/fingar/Arrangement.java
@@ -0,0 +1,10 @@
+package nruth.fingar;
+/**
+ * holds an individual solution to the guitar fingering problem
+ * stores tablature, timing and finger positions
+ * @author nicholasrutherford
+ *
+ */
+public class Arrangement {
+
+}
---
+++ nruth/fingar/FINGAR.java
@@ -0,0 +1,12 @@
+package nruth.fingar;
+
+/**
+ * this class sets the GA in motion
+ * it is responsible for creating the initial population and setting it running within an observer, which will report back when a solution is found
+ * using the observer model will allow plug-in expansion into a distributed GA 
+ * @author nicholasrutherford
+ *
+ */
+public class FINGAR {
+
+}
