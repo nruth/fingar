faf778ba49047285a99d8667834ea3176345a1d5
Nicholas Rutherford <nick.rutherford@ardes.com>
2009-02-02 20:54:26 +0000

    specced and red/greened on the GA initialiser

--- nruth/fingar/FINGAR.java
+++ nruth/fingar/FINGAR.java
@@ -1,5 +1,9 @@
 package nruth.fingar;
 
+import java.util.List;
+
+import nruth.fingar.domain.Score;
+
 /**
  * this class sets the GA in motion
  * it is responsible for creating the initial population and setting it running within an observer, which will report back when a solution is found
@@ -7,6 +11,29 @@ package nruth.fingar;
  * @author nicholasrutherford
  *
  */
-public class FINGAR {
+public final class FINGAR {
+	private final Score score;
+	private boolean finished = false;
+	
+	public FINGAR(Score score) {
+		this.score = score;
+	}
+
+	public List<Arrangement> getArrangements() {
+		if(!finished){	process();	}
+		
+		return null;
+	}
+
+	public Score getScore() {	return score;	}
+
+	public boolean is_processing_finished() {	return finished;	}
+
+	public void process() {
+		//1. do the processing here
+		//2. store the results
+		//3. update the processing done flag
+		finished = true;
+	}
 
 }
---
+++ nruth/fingar/tests/FINGAR_Spec.java
@@ -0,0 +1,54 @@
+package nruth.fingar.tests;
+
+import java.util.List;
+
+import nruth.fingar.Arrangement;
+import nruth.fingar.FINGAR;
+import nruth.fingar.domain.Score;
+import static org.junit.Assert.*;
+import org.junit.*;
+
+/**
+ * as a guitarist 
+ * I want FINGAR to find me the best fingering solutions
+ * so that I can play the piece comfortably and efficiently
+ * but I want it to be robust against me doing silly things, like running the same calculation twice or losing the results
+ */
+public class FINGAR_Spec {
+	private Score score;
+	private FINGAR ga;
+	
+	@Before 
+	public void remember_score_provided(){
+		score = nruth.fingar.domain.tests.ScoreSpec.get_test_score();
+		ga = new FINGAR(score);
+	}
+	
+	/**
+	 * ensure that after any test the score has remained the same, in case the interface is changed to allow it to be mutable
+	 */
+	@After
+	public void check_score_remains_same(){
+		assertEquals(score, ga.getScore());
+	}
+	
+	/**
+	 * when I create a FINGAR object with a musical score
+	 * 	it should know whether it has calculated a result or not, to avoid repeated calculation
+	 */
+	@Test
+	public void get_has_calculated_result(){
+		assertFalse(ga.is_processing_finished());
+		ga.process();
+		assertTrue(ga.is_processing_finished());
+	}
+
+	/**
+	 * when I ask for results I should be given a non-null list of arrangements
+	 */
+	@Test
+	public void results_give_non_null_list_of_arrangements(){
+		List<Arrangement> result = ga.getArrangements();
+		assertNotNull(result);
+	}
+}
