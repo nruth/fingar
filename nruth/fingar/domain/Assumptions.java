a976f2c6d3e6e87ac1c9f20cf93f78f28d45d4db
Nicholas Rutherford <nick.rutherford@ardes.com>
2009-02-04 03:42:20 +0000

    made changes to assumptions, for better use in arrangements & later refactoring to configurable/options

--- nruth/fingar/domain/Assumptions.java
+++ nruth/fingar/domain/Assumptions.java
@@ -8,8 +8,8 @@ package nruth.fingar.domain;
 	
  */
 public final class Assumptions {
-	public static final int FINGERS = 4;
-	public static final int STRINGS = 6;
+	public static final int[] FINGERS = {1,2,3,4};
+	public static enum STRINGS {LOW_E, A, D, G, B, HIGH_E}
 	public static final int FRETS = 24;
 	public static final int OCTAVE_RANGE = 4;
 }
