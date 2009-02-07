996c610beb8418962951fa41a9ef549a4af4f7fe
Nicholas Rutherford <nick.rutherford@ardes.com>
2009-02-07 10:33:19 +0000

    initial spec work

--- nruth/fingar/ga/specs/EvolverSpec.java
+++ nruth/fingar/ga/specs/EvolverSpec.java
@@ -1,5 +1,34 @@
 package nruth.fingar.ga.specs;
 
-public class EvolverSpec {
+import static org.junit.Assert.*;
 
+import org.junit.*;
+
+import nruth.fingar.ga.Population;
+import nruth.fingar.ga.evolvers.Evolver;
+import nruth.fingar.ga.evolvers.NDeepRandomEvolver;
+
+public class EvolverSpec {
+	/**
+	 * given a forebear population, can create a successor population 
+	 * which is atomically distinct from the forebear population (ignoring the contents)
+	 */
+	@Test
+	public void can_create_a_successor_population(){
+		Evolver evolver = test_evolver();
+		Population forebears = null;
+		assertNotSame(forebears, evolver.create_successor_population(forebears));
+	}
+	
+	/**
+	 * knows whether it has halted or is still processing
+	 */
+	@Test
+	public void knows_whether_halted_or_processing(){
+		assertFalse(new NDeepRandomEvolver(3).is_halted());
+		assertTrue(new NDeepRandomEvolver(0).is_halted());
+	}
+	
+	//helper factory
+	public static Evolver test_evolver(){ return new NDeepRandomEvolver(3); } 
 }
--- nruth/fingar/ga/specs/PopulationSpec.java
+++ nruth/fingar/ga/specs/PopulationSpec.java
@@ -1,5 +1,20 @@
 package nruth.fingar.ga.specs;
 
-public class PopulationSpec {
+import org.junit.Test;
+import nruth.fingar.domain.specs.ScoreSpec;
+import nruth.fingar.ga.Population;
+import static junit.framework.Assert.*;
 
+public class PopulationSpec {
+	/**
+	 * given an evolver, can produce a distinct successor population
+	 */
+	@Test
+	public void given_evolver_can_produce_successors(){
+		Population pop = test_population();
+		assertNotSame(pop, pop.successor());
+	}
+	
+	//helpers
+	public static Population test_population(){	return new Population(ScoreSpec.get_test_score(), EvolverSpec.test_evolver()); }
 }
