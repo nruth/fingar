9cf6b05e718892299ade56ffed6e0673b2ae9db1
Nicholas Rutherford <nick.rutherford@ardes.com>
2009-05-04 14:30:11 +0100

    improved graph plotting with titles etc

--- plot_graphs.m
+++ plot_graphs.m
@@ -1,15 +1,20 @@
 function plot_graphs(n)
-  meanplot = figure('Name', 'Mean Cost','NumberTitle', 'off');hold on
-  medianplot = figure('Name', 'Median Cost','NumberTitle', 'off');hold on
-  modalplot = figure('Name', 'Modal Cost','NumberTitle', 'off');hold on
-  minplot = figure('Name', 'Minimum Cost','NumberTitle', 'off');hold on
+  pop = loadfile(1); pop = length(pop(1,:));
+  meanplot = figure('Name', 'Mean Cost','NumberTitle', 'off');title(['Mean Cost (Population ',num2str(pop),', ',num2str(n),' runs)']); xlabel('Generation'); ylabel('Cost'); hold on;
+  medianplot = figure('Name', 'Median Cost','NumberTitle', 'off');title(['Median Cost (Population ',num2str(pop),', ',num2str(n),' runs)']); xlabel('Generation'); ylabel('Cost'); hold on;
+  modalplot = figure('Name', 'Modal Cost','NumberTitle', 'off');title(['Modal Cost (Population ',num2str(pop),', ',num2str(n),' runs)']); xlabel('Generation'); ylabel('Cost'); hold on;
+  minplot = figure('Name', 'Minimum Cost','NumberTitle', 'off');title(['Minimum Cost (Population ',num2str(pop),', ',num2str(n),' runs)']); xlabel('Generation'); ylabel('Cost'); hold on;
   for n = 1:n
-    prefix = 'run_popsummary_';
-    x = load(strcat(prefix,int2str(n)));
+    x = loadfile(n);
     figure(meanplot); plot(mean(x'));
     figure(modalplot); plot(mode(x'));
     figure(medianplot); plot(median(x'));
     figure(minplot); plot(min(x'));
     clear x;
   end
+end
+
+function data = loadfile(n)
+  prefix = 'run_popsummary_';
+  data = load([prefix,num2str(n)]);  
 end
