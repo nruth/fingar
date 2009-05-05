114c1528ff871fe246fc38960d87b76a97733f64
Nicholas Rutherford <nick.rutherford@ardes.com>
2009-05-05 14:39:32 +0100

    working state prior to branching for mutation mechanism change

--- plot_graphs.m
+++ plot_graphs.m
@@ -1,11 +1,19 @@
-function plot_graphs(reps, dataname)
-  tmp = loadfile(1); pop = length(tmp(1,:));  gens = length(tmp(:,1)); clear tmp;
+function plot_graphs(x, reps, dataname, filename)
+  cd(x)
+  plot_graphs_aux(reps, dataname, filename)
+end
+
+function plot_graphs_aux(reps, dataname, filename)
+  tmp = loadfile(filename, 1); 
+  pop = length(tmp(1,:))  
+  gens = length(tmp(:,1)) 
+  clear tmp;
   meanplot = figure('Name', 'Mean Cost','NumberTitle', 'off');title(['Mean Cost (Population ',num2str(pop),', ',num2str(reps),' runs)']); xlabel('Generation'); ylabel('Cost'); hold on;
   medianplot = figure('Name', 'Median Cost','NumberTitle', 'off');title(['Median Cost (Population ',num2str(pop),', ',num2str(reps),' runs)']); xlabel('Generation'); ylabel('Cost'); hold on;
   modalplot = figure('Name', 'Modal Cost','NumberTitle', 'off');title(['Modal Cost (Population ',num2str(pop),', ',num2str(reps),' runs)']); xlabel('Generation'); ylabel('Cost'); hold on;
   minplot = figure('Name', 'Minimum Cost','NumberTitle', 'off');title(['Minimum Cost (Population ',num2str(pop),', ',num2str(reps),' runs)']); xlabel('Generation'); ylabel('Cost'); hold on;
   for n = 1:reps
-    x = loadfile(n);
+    x = loadfile(filename, n);
     figure(meanplot); plot(mean(x'));
     figure(modalplot); plot(mode(x'));
     figure(medianplot); plot(median(x'));
@@ -13,14 +21,13 @@ function plot_graphs(reps, dataname)
     clear x;
   end
   
-  saveas(meanplot, [dataname,'_p',num2str(pop),'_g',gens,'_r',num2str(reps),'_mean'] ,'eps')
-  saveas(medianplot, [dataname,'_p',num2str(pop),'_g',gens,'_r',num2str(reps),'_median'] ,'eps')
-  saveas(modalplot, [dataname,'_p',num2str(pop),'_g',gens,'_r',num2str(reps),'_modal'] ,'eps')
-  saveas(minplot, [dataname,'_p',num2str(pop),'_g',gens,'_r',num2str(reps),'_min'] ,'eps')
+  saveas(meanplot, [dataname,'_p',num2str(pop),'_g',num2str(gens),'_r',num2str(reps),'_mean'] ,'eps')
+  saveas(medianplot, [dataname,'_p',num2str(pop),'_g',num2str(gens),'_r',num2str(reps),'_median'] ,'eps')
+  saveas(modalplot, [dataname,'_p',num2str(pop),'_g',num2str(gens),'_r',num2str(reps),'_mode'] ,'eps')
+  saveas(minplot, [dataname,'_p',num2str(pop),'_g',num2str(gens),'_r',num2str(reps),'_min'] ,'eps')
   
 end
 
-function data = loadfile(n)
-  prefix = 'run_popsummary_';
-  data = load([prefix,num2str(n)]);  
+function data = loadfile(filename, n)
+  data = load([filename,num2str(n)]);  
 end
--- produce_pdf_graphs.rb
+++ produce_pdf_graphs.rb
@@ -1,18 +1,22 @@
 #!/usr/bin/env ruby -wKU
 
-raise ArgumentError unless dataname = ARGV[0]  
+raise ArgumentError unless sourcefilename = ARGV[0]
 raise ArgumentError unless runs = ARGV[1]
+raise ArgumentError unless dataname = ARGV[2]
+raise ArgumentError unless dir = ARGV[3]  
 
 puts "Making Graphs with MATLAB"
 cmd =
-"plot_graphs(#{runs}, '#{dataname}')
+"plot_graphs('#{dir}', #{runs}, '#{dataname}', '#{sourcefilename}')
 exit"
 puts cmd
 puts `echo "#{cmd}" | matlab`
 
-puts "Moving the files into a sorted location"
-dir = "#{dataname}"
-raise Exception unless `mkdir #{dir}` 
-raise Exception unless `mv #{dataname}*.eps #{dir}`
+#the files are already there now so no need for this.
+# puts "Moving the files into a sorted location"
+# dir = "#{dataname}"
+# raise Exception unless `mkdir #{dir}` 
+# raise Exception unless `mv #{dataname}*.eps #{dir}`
+
 #Convert the graph eps to pdf for pdftex compatibility
-`cd #{dir} && ../epsconvert.rb`
+puts `cd #{dir} && ../epsconvert.rb`
